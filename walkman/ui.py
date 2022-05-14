745dae540056176a37fcc34d737a1b3d41bbc89f
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-05-14 13:44:24 +0200

    ui: Add Select soundfile and improve code

--- walkman/ui.py
+++ walkman/ui.py
@@ -1,57 +1,59 @@
 import abc
+import dataclasses
+import functools
 import typing
 
-import pynput
 import PySimpleGUI as sg
 
 import walkman
 
 
+@dataclasses.dataclass(frozen=True)
 class UIElement(abc.ABC):
-    def __init__(
-        self,
-        audio_host: walkman.audio.AudioHost,
-        keyboard_key: typing.Optional[pynput.keyboard.Key] = None,
-    ):
-        self.audio_host = audio_host
-        self.keyboard_key = keyboard_key
+    audio_host: walkman.audio.AudioHost
+    key_tuple: typing.Optional[str] = None
+    keyboard_key_tuple: typing.Optional[str] = None
 
     @property
-    @abc.abstractmethod
-    def gui_element(self):
-        ...
+    def gui_element(self) -> typing.Optional[typing.Union[list, sg.Element]]:
+        return None
 
     @abc.abstractmethod
     def handle_event(self, event: str, value_dict: dict):
         ...
 
-    def handle_keyboard(self, is_pressed: bool):
-        pass
+    @functools.cached_property
+    def event_tuple(self) -> typing.Tuple[str, ...]:
+        event_list = []
+        if self.key_tuple:
+            event_list.extend(self.key_tuple)
+        if self.keyboard_key_tuple:
+            event_list.extend(self.keyboard_key_tuple)
+        return tuple(event_list)
 
 
 class Button(UIElement):
-    def __init__(self, name: str, *args, button_kwargs={}, **kwargs):
-        self.name = name
+    def __init__(self, *args, button_kwargs={}, **kwargs):
         self.button_kwargs = button_kwargs
         super().__init__(*args, **kwargs)
 
-    @property
-    def gui_element(self):
-        return sg.Button(self.name, **self.button_kwargs)
+    @functools.cached_property
+    def gui_element(self) -> typing.Optional[typing.Union[list, sg.Element]]:
+        return sg.Button(key=self.key_tuple[0], **self.button_kwargs)
 
 
 class StartStopButton(Button):
     def __init__(self, *args, **kwargs):
         super().__init__(
-            "start_stop",
             *args,
             button_kwargs={},
-            keyboard_key=pynput.keyboard.Key.space,
-            **kwargs
+            key_tuple=("start_stop",),
+            keyboard_key_tuple=("space:65",),
+            **kwargs,
         )
         self.is_playing = 0
 
-    def _handle(self):
+    def handle_event(self, _: str, __: dict):
         if self.is_playing:
             self.audio_host.stop()
         else:
@@ -60,49 +62,98 @@ class StartStopButton(Button):
                 self.audio_host.sound_file_player.play()
         self.is_playing = not self.is_playing
 
-    def handle_event(self, event: str, _: dict):
-        if event == self.name:
-            self._handle()
 
-    def handle_keyboard(self, is_pressed: bool):
-        if is_pressed:
-            self._handle()
+class SelectSoundFileMenu(UIElement):
+    def __init__(self, *args, **kwargs):
+        self.combo_key = "select_sound_file"
+
+        self.left_key = "Left:113"
+        self.right_key = "Right:114"
+
+        super().__init__(
+            *args,
+            key_tuple=(self.combo_key,),
+            keyboard_key_tuple=(self.left_key, self.right_key),
+            **kwargs,
+        )
+
+        self.name_to_sound_file_dict = {
+            sound_file.name: sound_file
+            for sound_file in self.audio_host.sound_file_tuple
+        }
+        self.value_tuple = tuple(self.name_to_sound_file_dict.keys())
+        self.value_count = len(self.value_tuple)
+
+    @functools.cached_property
+    def gui_element(self) -> typing.Optional[typing.Union[list, sg.Element]]:
+        return sg.Combo(
+            self.value_tuple,
+            default_value=self.value_tuple[0],
+            readonly=True,
+            enable_events=True,
+            key=self.combo_key,
+        )
+
+    def handle_event(self, event: str, value_dict: dict):
+        sound_file_name = value_dict[self.combo_key]
+        if event != self.combo_key:
+            sound_file_index = self.value_tuple.index(sound_file_name)
+            new_sound_file_index = (
+                sound_file_index + {self.left_key: -1, self.right_key: 1}[event]
+            ) % self.value_count
+            sound_file_name = self.value_tuple[new_sound_file_index]
+            self.gui_element.update(value=sound_file_name)
+        sound_file = self.name_to_sound_file_dict[sound_file_name]
+        self.audio_host.sound_file_player.sound_file = sound_file
+
+
+class DuplicateEventError(Exception):
+    def __init__(self, event: str):
+        super().__init__(f"Event '{event}' has been defined twice!")
 
 
 class GUI(object):
-    def __init__(self, ui_element_sequence: typing.Sequence[UIElement]):
+    def __init__(
+        self,
+        audio_host: walkman.audio.AudioHost,
+        ui_element_sequence: typing.Sequence[UIElement],
+        theme: str = "DarkBlack",
+    ):
+        sg.theme(theme)
+
+        self.audio_host = audio_host
         self.ui_element_tuple = tuple(ui_element_sequence)
-        self.keyboard_key_to_ui_element = {
-            ui_element.keyboard_key: ui_element
-            for ui_element in self.ui_element_tuple
-            if ui_element.keyboard_key is not None
-        }
+        event_to_ui_element_dict = {}
+        for ui_element in self.ui_element_tuple:
+            for event in ui_element.event_tuple:
+                if event not in event_to_ui_element_dict:
+                    event_to_ui_element_dict.update({event: ui_element})
+                else:
+                    raise DuplicateEventError(event)
+        self.event_to_ui_element_dict = event_to_ui_element_dict
 
     @property
     def layout(self) -> list:
-        return [[ui_element.gui_element for ui_element in self.ui_element_tuple]]
+        return [
+            [
+                ui_element.gui_element
+                for ui_element in self.ui_element_tuple
+                if ui_element.gui_element
+            ]
+        ]
 
-    def handle_keyboard_event(
-        self, keyboard_key: pynput.keyboard.Key, is_pressed: bool
-    ):
+    def handle_event(self, event: str, value_dict: dict):
         try:
-            ui_element = self.keyboard_key_to_ui_element[keyboard_key]
+            ui_element = self.event_to_ui_element_dict[event]
         except KeyError:
             pass
-        return ui_element.handle_keyboard(is_pressed)
+        else:
+            return ui_element.handle_event(event, value_dict)
 
     def loop(self):
-        window = sg.Window(walkman.constants.NAME, self.layout)
-
-        keyboard_listener = pynput.keyboard.Listener(
-            on_press=lambda keyboard_key: self.handle_keyboard_event(
-                keyboard_key, True
-            ),
-            on_release=lambda keyboard_key: self.handle_keyboard_event(
-                keyboard_key, False
-            ),
+        window = sg.Window(
+            walkman.constants.NAME, self.layout, return_keyboard_events=True
         )
-        keyboard_listener.start()
 
         while True:
             event, value_dict = window.read()
@@ -111,15 +162,21 @@ class GUI(object):
             if event == sg.WINDOW_CLOSED or event == "Quit":
                 break
 
-            for ui_element in self.ui_element_tuple:
-                ui_element.handle_event(event, value_dict)
+            walkman.constants.LOGGER.debug(
+                f"Catched event       = '{event}' \n"
+                f"with value_dict     = '{value_dict}'\n."
+            )
+            self.handle_event(event, value_dict)
 
+        self.audio_host.close()
         window.close()
 
 
-UI_CLASS_TUPLE = (StartStopButton,)
+UI_CLASS_TUPLE = (StartStopButton, SelectSoundFileMenu)
 
 
 def audio_host_to_gui(audio_host: walkman.audio.AudioHost) -> GUI:
-    gui = GUI([ui_class(audio_host) for ui_class in UI_CLASS_TUPLE])
+    gui = GUI(
+        audio_host, [ui_class(audio_host=audio_host) for ui_class in UI_CLASS_TUPLE]
+    )
     return gui
