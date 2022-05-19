67cb668d3efe172960a5f411bc621f52f4ae64cf
levinericzimmermann <levin.eric.zimmermann@posteo.eu>
2022-05-19 16:40:50 +0200

    Bump to version 0.3.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 import setuptools  # type: ignore
 
-MAJOR, MINOR, PATCH = 0, 2, 0
+MAJOR, MINOR, PATCH = 0, 3, 0
 VERSION = f"{MAJOR}.{MINOR}.{PATCH}"
 """This project uses semantic versioning.
 Please consult the following page for more information:
