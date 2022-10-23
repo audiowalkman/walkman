bd27502d0373028a82e5f401d472c840460eb41a
Levin Eric Zimmermann <levin.eric.zimmermann@posteo.eu>
2022-10-23 16:45:53 +0200

    Cosmetics

--- tests/automatic/base_tests.py
+++ tests/automatic/base_tests.py
@@ -11,7 +11,7 @@ class BaseTest(walkman.unit_tests.WalkmanTestCase, unittest.TestCase):
     def test_sine_audio_object(self):
         sine_audio_object = walkman.unit_tests.SineAudioObject()
         sine_audio_object.pyo_object.play()
-        self.assertTrue(self.is_pyo_object_not_silent(sine_audio_object.pyo_object))
+        self.assertIsNotSilent(sine_audio_object.pyo_object)
 
     def test_is_pyo_object_not_silent(self):
         sine = pyo.Sine()
@@ -25,5 +25,4 @@ class BaseTest(walkman.unit_tests.WalkmanTestCase, unittest.TestCase):
     def test_is_pyo_object_silent(self):
         sine = pyo.Sine(mul=0)
         sine.play()
-        is_pyo_object_silent = self.is_pyo_object_silent(sine)
-        self.assertTrue(is_pyo_object_silent)
+        self.assertIsSilent(sine)
